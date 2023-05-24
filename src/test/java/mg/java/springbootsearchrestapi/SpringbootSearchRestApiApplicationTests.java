43ba117522c791c2183398ca81f73a63d4e80d4f
mgace <matejgace@gmail.com>
2023-05-24 12:50:28 +0200

    Initial commit

---
+++ src/test/java/mg/java/springbootsearchrestapi/SpringbootSearchRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.java.springbootsearchrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSearchRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
